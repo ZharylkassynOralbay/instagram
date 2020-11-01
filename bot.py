b3427ca418eaec85b892ead7257a1306ffeb6333
Zharylkassyn Oralbay <lazzatal.uk@gmail.com>
2020-11-01 14:22:28 +0300

    1

---
+++ bot.py
@@ -0,0 +1,14 @@
+from instapy import InstaPy
+
+
+session = InstaPy(username="learn_earn_travel_enjoy", password="0314007kZ")
+session.login()
+
+session.like_by_tags(['саяхат', 'английский'], amount=6)
+
+session.set_do_follow(True, percentage=25)
+
+session.session.set_do_comment(True,percentage=50)
+session.set_comments(['Love it','Nice post', u'Kushti 😍'])
+
+session.end()
